f30e958ebfca81aa8eb6c01bb1c0789a5da2dcbc
stefanyfariasm <snfarias@espol.edu.ec>
2023-06-07 23:52:24 -0500

    archivo xml y reportes

--- coding_Standar_FariasMera/src/main/java/coding_Standar_FariasMera/VacationPackageCalculator.java
+++ coding_Standar_FariasMera/src/main/java/coding_Standar_FariasMera/VacationPackageCalculator.java
@@ -1,13 +1,20 @@
 package coding_Standar_FariasMera;
 
 import java.util.Scanner;
-
+/**
+ * 
+ * @author stefanynfm
+ *
+ */
 public class VacationPackageCalculator {
+	/**
+	 * 
+	 * @author stefanynfm
+	 *
+	 */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Obtener la información del usuario
-        System.out.print("Ingrese el destino de las vacaciones (Paris, New York City): ");
+        System.out.print("Ingrese el destino de las vacaciones: ");
         String destination = scanner.nextLine();
 
         System.out.print("Ingrese el número de viajeros: ");
@@ -16,36 +23,35 @@ public class VacationPackageCalculator {
         System.out.print("Ingrese la duración de las vacaciones en días: ");
         int duration = scanner.nextInt();
 
-        // Calcular el costo base
         int totalCost = 1000;
 
-        // Verificar si el destino es un lugar turístico popular y agregar el costo adicional
-        if (destination.equalsIgnoreCase("Paris")) {
+        if ("Paris".equalsIgnoreCase(destination)) {
             totalCost += 500;
-        } else if (destination.equalsIgnoreCase("New York City")) {
+        } else if ("New York City".equalsIgnoreCase(destination)) {
             totalCost += 600;
         }
 
-        // Verificar el descuento de grupo
         if (numOfTravelers > 4 && numOfTravelers < 10) {
             totalCost -= totalCost * 0.1;
         } else if (numOfTravelers > 10) {
             totalCost -= totalCost * 0.2;
         }
 
-        // Verificar la duración de las vacaciones y agregar o restar los costos correspondientes
         if (duration < 7) {
             totalCost += 200;
         } else if (duration > 30 || numOfTravelers == 2) {
             totalCost -= 200;
         }
-
-        // Verificar si el número de personas excede el límite de 80
+        
+        /**
+         * 
+         * @author stefanynfm
+         *
+         */
         if (numOfTravelers > 80) {
             System.out.println("El paquete de vacaciones no está disponible para grupos de más de 80 personas.");
             totalCost = -1;
         } else {
-            // Obtener la selección de los complementos opcionales
             System.out.print("¿Desea agregar el paquete Todo Incluido? (Sí/No): ");
             String allInclusiveChoice = scanner.next();
 
@@ -55,21 +61,24 @@ public class VacationPackageCalculator {
             System.out.print("¿Desea agregar el paquete de Spa? (Sí/No): ");
             String spaPackageChoice = scanner.next();
 
-            // Calcular el costo de los complementos seleccionados
-            if (allInclusiveChoice.equalsIgnoreCase("Sí")) {
+            if ("Sí".equalsIgnoreCase(allInclusiveChoice)) {
                 totalCost += 200 * numOfTravelers;
             }
 
-            if (adventurePackageChoice.equalsIgnoreCase("Sí")) {
+            if ("Sí".equalsIgnoreCase(adventurePackageChoice)) {
                 totalCost += 150 * numOfTravelers;
             }
 
-            if (spaPackageChoice.equalsIgnoreCase("Sí")) {
+            if ("Sí".equalsIgnoreCase(spaPackageChoice)) {
                 totalCost += 100 * numOfTravelers;
             }
         }
-
-        // Mostrar el costo total o el mensaje de error si corresponde
+        
+        /**
+         * 
+         * @author stefanynfm
+         *
+         */
         if (totalCost != -1) {
             System.out.println("El costo total del paquete de vacaciones es: $" + totalCost);
         } else {
@@ -77,4 +86,3 @@ public class VacationPackageCalculator {
         }
     }
 }
-
